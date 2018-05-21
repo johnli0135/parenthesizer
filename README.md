63780332bd3d67e274b982fe3736b774b5ee72d3
John Li <johnli0135@gmail.com>
2018-05-20 23:28:41 -0400

    first

---
+++ README.md
@@ -0,0 +1,69 @@
+# Parenthesizer
+
+A command line utility that uses whitespace and punctuation rules to generate properly parenthesized code of LISP dialects.
+
+## Directives
+
+Knowledge of operator arity is given through directives. A directive is any line where the first non-whitespace character is a slash.
+
+`/use f`: process a file `f` and include the results in the current file.
+
+`/def f [arity]`: define an operator `f` to take `arity` arguments, or to take a variable number of arguments if `arity` is not specified.
+
+`//`: line comment.
+
+## Punctuation rules
+
+A comma, semicolon, or period can serve as a closing parenthesis for an operator that takes a variable number of arguments:
+
+```C
+/use racket.txt
+define (f x y) 
+    display x;
+    display y;
+    + x y.
+```
+
+A colon at the end of an operator name forces the operator to take a variable number of arguments:
+
+```
++: 1 2 3.
+```
+
+## Whitespace rule
+
+Going from a higher indentation to lower indentation closes any blocks in between (if they exist).
+
+End-of-file is treated as a blank line of lowest possible indentation level.
+
+Indentations are preserved after addition of parentheses.
+
+```C
+/use racket.txt
+define (f x y)
+    +
+        x
+      // does nothing, since no operators above this indentation level
+      y
+// lower indentation level closes define and + 
+```
+will generate
+```racket
+(define (f x y)
+    (+
+        x
+      y))
+```
+
+An esoteric variant of the first punctuation example:
+
+```C
+/use racket.txt
+define (f x y)
+    display x
+   // trigger a closing parenthesis for display x
+   display y
+  // trigger a closing parenthesis for display y
+  + x y
+// close the define
+```
---
+++ parenthesizer.py
@@ -0,0 +1,209 @@
+import sys
+
+def lex(line, symbols):
+  # \ is the universal escape character (the next char is always interpreted verbatim)
+  # recognize strings and braces and symbols (numbers are just symbols)
+  DEFAULT = 0
+  IDENTIFIER = 1
+  STRING = 2
+  ESCAPE = 3
+  SYMBOL = 4
+
+  token = []
+  symbol = [""]
+  ret = [DEFAULT]
+
+  def reset_token(c):
+    del token[:]
+    token.append(c)
+
+  def lex_default(c):
+    if c in symbols:
+      symbol[0] = c
+      return SYMBOL, False
+    if c == "\\":
+      ret[0] = DEFAULT
+      return ESCAPE, False
+    if c == '"':
+      reset_token(c)
+      return STRING, False
+    if c.isspace():
+      return DEFAULT, False
+    reset_token(c)
+    return IDENTIFIER, False
+
+  def lex_identifier(c):
+    if c in symbols:
+      symbol[0] = c
+      return SYMBOL, True
+    if c == "\\":
+      ret[0] = IDENTIFIER
+      return ESCAPE, False
+    if c.isspace():
+      return DEFAULT, True
+    token.append(c)
+    return IDENTIFIER, False
+
+  def lex_string(c):
+    if c == "\\":
+      ret[0] = STRING
+      token.append("\\")
+      return ESCAPE, False
+    if c == "\"":
+      token.append("\"")
+      return DEFAULT, True
+    token.append(c)
+    return STRING, False
+
+  def lex_escape(c):
+    token.append(c)
+    return ret[0], False
+
+  def lex_symbol(c):
+    pass
+
+  state = DEFAULT
+  dfa = {
+    DEFAULT: lex_default,
+    IDENTIFIER: lex_identifier,
+    STRING: lex_string,
+    ESCAPE: lex_escape,
+    SYMBOL: lex_symbol
+  }
+  for c in line + "\n":
+    state, gottoken = dfa[state](c)
+    if gottoken:
+      yield "".join(token)
+    if state == SYMBOL:
+      yield symbol[0]
+      state = DEFAULT
+
+def parenthesize(lines):
+  variadic = -1
+  prefix = "/"
+  closing_punctuation = ",;."
+  braces = "{}()[]"
+  indentation = "    "
+
+  result = []
+
+  bindings = {} # maps identifiers to arities (-1 if variadic)
+  scopes = {} # maps indentation levels to operators
+  levels = [] # list of indentation levels with keys in scopes
+  demands = [] # the number arguments left to satisfy the last operator on the call stack (negative if variadic)
+
+  def write(s):
+    result[-1] += s
+
+  def newline():
+    result.append("")
+
+  def buffer():
+    return result[-1]
+
+  # create a new demand
+  def demand(arity, indent_level):
+    demands.append(arity)
+    if indent_level in scopes:
+      scopes[indent_level] += 1
+    else:
+      scopes[indent_level] = 1
+      levels.append(indent_level)
+
+  # close a demand
+  def resolve():
+    write(")")
+    demands.pop()
+
+    level = levels[-1]
+    scopes[level] -= 1
+    if scopes[level] == 0:
+      del scopes[level]
+      levels.pop()
+
+    if len(demands) > 0:
+      appease()
+
+  # appease a demand
+  def appease():
+    if demands[-1] == variadic:
+      return
+
+    demands[-1] -= 1
+    if demands[-1] == 0:
+      resolve()
+
+  def deindent(old, new):
+    while len(levels) > 0 and levels[-1] >= new:
+      resolve()
+
+  # directives
+  def use(filename):
+    r, b = parenthesize(open(filename))
+    result.extend(r)
+    for binding in b:
+      bindings[binding] = b[binding]
+
+  def define(name, arity=variadic):
+    bindings[name] = int(arity)
+
+  def comment(*args):
+    pass
+
+  directives = {
+    "use": use,
+    "def": define,
+    "/": comment
+  }
+
+  last_indent = None
+  for l in lines:
+    # extract indentation level and code
+    line = l.rstrip()
+    code = line.lstrip()
+    indent = len(line) - len(code)
+
+    if last_indent is not None and indent < last_indent:
+      deindent(last_indent, indent)
+    last_indent = indent
+
+    # if directive, handle it and skip this line
+    if code.startswith(prefix):
+      tokens = code[len(prefix):].split(" ")
+      directive = tokens[0]
+      arguments = tokens[1:]
+      directives[directive](*arguments)
+      continue
+
+    newline()
+    write(" " * indent)
+    for token in lex(code, closing_punctuation): # include braces? harder to force spaces in output that way
+      #print(token, bindings)
+      #print(result, demands, scopes)
+      # normal binding
+      if token in bindings:
+        if buffer() != "":
+          write(" ")
+        write("(" + token)
+        demand(bindings[token], indent)
+      # forced variadic binding
+      elif token[-1] == ":" and token[:-1] in bindings:
+        if buffer() != "":
+          write(" ")
+        write("(" + token[:-1])
+        demand(variadic, indent)
+      # close a binding
+      elif token in closing_punctuation:
+        resolve()
+      # normal token
+      else:
+        write(" " + token)
+        appease()
+
+  deindent(indent, 0)
+
+  return result, bindings
+
+source = sys.argv[1]
+result, bindings = parenthesize(open(source))
+print("\n".join(result))
